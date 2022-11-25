fe8198004f678fcee6a4aa9c687d92334a6f8114
Gumse <gumse@gumse.de>
2022-11-25 05:27:18 +0100

    minimal window

--- implementation/client/src/main/java/com/swp/KarteikartenAG.java
+++ implementation/client/src/main/java/com/swp/KarteikartenAG.java
@@ -1,18 +1,42 @@
 package com.swp;
 
+import static org.lwjgl.opengl.GL11.*;
+
+import com.gumse.basics.Globals;
+import com.gumse.maths.ivec2;
+import com.gumse.maths.vec4;
+import com.gumse.system.Display;
+import com.gumse.system.Window;
+import com.gumse.tools.FPS;
 import com.swp.GUI.KarteikartenAGGUI;
 
 public class KarteikartenAG 
 {
-    static boolean bRunning = true;
     public static void main(String[] args)
     {
+        Globals.DEBUG_BUILD = true;
+        System.setProperty("java.awt.headless", "true"); //for iCrap support
+
+        Display.init();
+        Window pMainWindow = new Window("Example App", new ivec2(500, 500), Window.GUM_WINDOW_RESIZABLE, null);
+        pMainWindow.setClearColor(new vec4(0.09f, 0.1f, 0.11f, 1.0f)); // Set the clear color);
+
+
         KarteikartenAGGUI pMainGUI = new KarteikartenAGGUI();
         
-        while(bRunning)
+        while(pMainWindow.isOpen())
         {
-            //pMainGUI.update();
-            //Render pMainGUI
+            Display.pollEvents();
+            pMainWindow.clear(GL_COLOR_BUFFER_BIT);
+
+            //System.out.println("Mouse is: " + (Mouse.isBusy() ? "Busy" : "Available"));
+
+            pMainWindow.finishRender();
+            pMainWindow.getMouse().reset();
+            pMainWindow.getKeyboard().reset();
+
+            //pMainWindow.update();
+            FPS.update();
         }
     }
 }
