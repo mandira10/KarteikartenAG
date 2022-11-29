1a712a9d1f6a04dd6ee2b89362fbc22bec397f50
mandira10 <efemandira@gmail.com>
2022-11-29 15:05:20 +0100

    add settings

--- implementation/client/src/main/java/com/swp/GUI/Settings/ExportSettingsPage.java
+++ implementation/client/src/main/java/com/swp/GUI/Settings/ExportSettingsPage.java
@@ -1,5 +1,51 @@
 package com.swp.GUI.Settings;
 
 public class ExportSettingsPage {
-    
+
+
+    private String language;
+    private boolean darkTheme;
+    private String serverAdress;
+    private int serverPort;
+    private static ExportSettingsPage settingsInstance;
+
+    private ExportSettingsPage() {
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public boolean isDarkTheme() {
+        return darkTheme;
+    }
+
+    public void setDarkTheme(boolean darkTheme) {
+        this.darkTheme = darkTheme;
+    }
+
+    public String getServerAdress() {
+        return serverAdress;
+    }
+
+    public void setServerAdress(String serverAdress) {
+        this.serverAdress = serverAdress;
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
+    public void setServerPort(int serverPort) {
+        this.serverPort = serverPort;
+    }
+
+    public static ExportSettingsPage getSettingsInstance() {
+        return settingsInstance;
+    }
+
 }
