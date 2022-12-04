d04382f387d986a3baa2f3ab407a7830d2298bb2
Ole-Niklas Mahlstädt <olma@uni-bremen.de>
2022-12-04 21:34:50 +0100

    Repositories im UML anpassen
    und trailing whitespace in den .java entfernt

--- implementation/client/src/main/java/com/swp/Persistence/DeckRepository.java
+++ implementation/client/src/main/java/com/swp/Persistence/DeckRepository.java
@@ -18,7 +18,7 @@ public class DeckRepository
     }
 
     /**
-     * 
+     *
      * @param olddeck
      * @param newdeck
      */
@@ -34,25 +34,25 @@ public class DeckRepository
         //server.send("/deletedeck", jsonString);
         return false;
     }
-    
-    public static Set<Deck> getDecks()  
-    { 
+
+    public static Set<Deck> getDecks()
+    {
         Set<Deck> decks = Cache.getInstance().getDecks();
         if(!decks.isEmpty())
             return decks;
 
         //server.send("/getdecks", jsonString);
-        return null; 
+        return null;
     }
 
-    public static Set<CardToDeck> getCardToDecks()  
-    { 
+    public static Set<CardToDeck> getCardToDecks()
+    {
         Set<CardToDeck> decks = Cache.getInstance().getCardToDecks();
         if(!decks.isEmpty())
             return decks;
 
         //server.send("/getcardtodecks", jsonString);
-        return null; 
+        return null;
     }
 
     //
@@ -65,32 +65,32 @@ public class DeckRepository
         return false;
     }
 
-    public static boolean addStudySystemType(StudySystemType type)                  
-    { 
+    public static boolean addStudySystemType(StudySystemType type)
+    {
         //True if successfully added to cache
         return false;
     }
 
-    public static boolean updateStudySystemTypes()                                  
-    { 
+    public static boolean updateStudySystemTypes()
+    {
 
         //server.send("/updatestudysystemtypes", jsonString);
         return false;
     }
     
     public static StudySystem getStudySystem(Deck deck)
-    { 
+    {
         //server.send("/getstudysystem", jsonString);
-        return null; 
+        return null;
     }
     
-    public static Set<StudySystemType> getStudySystemTypes()  
-    { 
+    public static Set<StudySystemType> getStudySystemTypes()
+    {
         Set<StudySystemType> types = Cache.getInstance().getStudySystemTypes();
         if(!types.isEmpty())
             return types;
 
         //server.send("/getstudysystemtypes", jsonString);
-        return null; 
+        return null;
     }
 }
