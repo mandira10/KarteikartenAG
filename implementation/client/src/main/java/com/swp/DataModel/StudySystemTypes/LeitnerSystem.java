3a697e752aa082cadfcc6b8638bee26144238af3
Mert As <meras@uni-bremen.de>
2022-12-20 17:38:38 +0100

    Lernensystem Pseudo Code

--- implementation/client/src/main/java/com/swp/DataModel/StudySystemTypes/LeitnerSystem.java
+++ implementation/client/src/main/java/com/swp/DataModel/StudySystemTypes/LeitnerSystem.java
@@ -17,6 +17,40 @@ public class LeitnerSystem extends StudySystem
     public LeitnerSystem(Deck deck)
     {
         super(deck, new StudySystemType(StudySystemType.KNOWN_TYPES.LEITNER), 5);
+        /**
+         for(int i = 0 ; i < size of cards that we will lernen;){
+             getNextCard(); // We can use getNextCard with index
+             String answer; // get answer from user
+             String realAnswer; // real answer from database
+             giveAnswer((realAnswer.equals(answer))); // We can send also index of card to this function
+             if(answer.equals(realAnswer)){
+                i++;
+            }
+            else{
+                if(i == 0){
+                    i == 0;
+                }
+                else{
+                    i--;
+                }
+            }
+         }
+
+
+         */
         
     }
+
+    /**
+     @Override
+     public void giveAnswer(boolean answer){
+         if(answer){
+             Show user that answer was True
+             Update Cards priority in Datebase
+         }
+         else{
+             Show user that answer was False
+             Update Cards priority in Datebase
+         }
+     */
 }
