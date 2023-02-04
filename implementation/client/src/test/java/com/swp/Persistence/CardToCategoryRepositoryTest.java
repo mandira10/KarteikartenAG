7713e39003a59a25f9ee218cc2f82627c1758377
Ole-Niklas Mahlstädt <olma@uni-bremen.de>
2023-02-04 12:42:01 +0100

    fix CardToCategory tests

--- implementation/client/src/test/java/com/swp/Persistence/CardToCategoryRepositoryTest.java
+++ implementation/client/src/test/java/com/swp/Persistence/CardToCategoryRepositoryTest.java
@@ -1,16 +1,17 @@
 package com.swp.Persistence;
 
-import com.gumse.textures.Texture;
 import com.swp.Controller.ControllerThreadPool;
-import com.swp.DataModel.*;
-import com.swp.DataModel.CardTypes.*;
+import com.swp.DataModel.Card;
+import com.swp.DataModel.CardToCategory;
+import com.swp.DataModel.CardTypes.AudioCard;
+import com.swp.DataModel.CardTypes.ImageDescriptionCard;
+import com.swp.DataModel.CardTypes.ImageTestCard;
+import com.swp.DataModel.CardTypes.TrueFalseCard;
+import com.swp.DataModel.Category;
 import com.swp.DataModel.Language.German;
-import com.swp.DataModel.StudySystem.BoxToCard;
-import com.swp.DataModel.StudySystem.LeitnerSystem;
-import com.swp.DataModel.StudySystem.StudySystem;
-import com.swp.DataModel.StudySystem.StudySystemBox;
 import jakarta.persistence.NoResultException;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -22,6 +23,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CardToCategoryRepositoryTest {
     // Repositories die getestet werden
     private final CardToCategoryRepository cardToCategoryRepository = CardToCategoryRepository.getInstance();
+    private final List<Card> cards = exampleCards();
+    private final List<Category> categories = exampleCategories();
 
     @BeforeAll
     public static void before()
@@ -30,10 +33,32 @@ public class CardToCategoryRepositoryTest {
         German.getInstance().activate();
         ControllerThreadPool.getInstance().synchronizedTasks(true);
     }
+
+    @BeforeEach
+    public void beforeEach() {
+        BaseRepository.startTransaction();
+        CardRepository.getInstance().delete(CardRepository.getInstance().getAll());
+        CategoryRepository.getInstance().delete(CategoryRepository.getInstance().getAll());
+        BaseRepository.commitTransaction();
+
+        CardToCategoryRepository.startTransaction();
+        cardToCategoryRepository.delete(cardToCategoryRepository.getAll());
+
+        for (Card card : cards) {
+            for (Category category : categories) {
+                cardToCategoryRepository.save(new CardToCategory(card, category));
+            }
+        }
+        CardToCategoryRepository.commitTransaction();
+    }
+
     @Test
     public void cardToCategoryCrudTest() {
-        List<Card> cards = exampleCards();
-        List<Category> categories = exampleCategories();
+        // DELETE (all)
+        CardToCategoryRepository.startTransaction();
+        assertDoesNotThrow(() -> cardToCategoryRepository.delete(cardToCategoryRepository.getAll()));
+        assertEquals(0, cardToCategoryRepository.countAll());
+        CardToCategoryRepository.commitTransaction();
 
         // CREATE
         CardToCategoryRepository.startTransaction();
@@ -49,35 +74,46 @@ public class CardToCategoryRepositoryTest {
         CardToCategoryRepository.startTransaction();
         for (final Card card : cards) {
             for (final Category category : categories) {
-                //final CardToCategory readCard = cardToCategoryRepository.//getCardToCategoryByUUID(card.getUuid(),
-                        //category.getUuid());
-                //assertEquals(card, readCard.getCard());
-                //assertEquals(category, readCard.getCategory());
-                //allReadCards.add(readCard);
+                final CardToCategory readCardToCategory = cardToCategoryRepository.getSpecific(card, category);
+                assertEquals(card, readCardToCategory.getCard());
+                assertEquals(category, readCardToCategory.getCategory());
+                allReadCards.add(readCardToCategory);
             }
         }
-        CardToCategoryRepository.commitTransaction();
         assertEquals(cards.size() * categories.size(), allReadCards.size(), "same length");
-        assertTrue(allReadCards.containsAll(cards));
+        assertTrue(allReadCards.stream().map(CardToCategory::getCard).toList().containsAll(cards));
+        CardToCategoryRepository.commitTransaction();
 
         // UPDATE
-        // TODO
+        // alle Attribute sind final, man kann nur löschen oder neue Verbindungen anlegen.
 
-        // DELETE
-        // TODO
-
+        // DELETE (specific)
+        CardToCategoryRepository.startTransaction();
+        CardToCategory toDelete = cardToCategoryRepository.getSpecific(cards.get(0), categories.get(0));
+        assertDoesNotThrow(() -> cardToCategoryRepository.delete(toDelete));
+        assertThrows(NoResultException.class, () -> cardToCategoryRepository.getSpecific(cards.get(0), categories.get(0)));
+        CardToCategoryRepository.commitTransaction();
     }
 
     @Test
     public void getAllC2CForCategory() {
+        CardRepository.startTransaction();
+        assertEquals(CardRepository.getInstance().getCardsByCategory(categories.get(0)).size(), cards.size());
+        CardRepository.commitTransaction();
     }
 
     @Test
     public void getSpecific() {
+        CardToCategoryRepository.startTransaction();
+        assertNotNull(cardToCategoryRepository.getSpecific(cards.get(2), categories.get(1)));
+        CardToCategoryRepository.commitTransaction();
     }
 
     @Test
     public void getAllC2CForCard() {
+        CategoryRepository.startTransaction();
+        assertEquals(CategoryRepository.getInstance().getCategoriesToCard(cards.get(0)).size(), categories.size());
+        CategoryRepository.commitTransaction();
     }
 
     private List<Card> exampleCards() {
